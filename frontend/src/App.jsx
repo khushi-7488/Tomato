04b5affd55befcc04af20174d9a34982a99081f2
khushi-7488 <khushiitiwari88@gmail.com>
2026-03-12 23:27:58 +0530

    build header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./Header.css";
+import { assets } from "../../assets/assets";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="header">
+        <div className="header-contents">
+          <h2>Order your favourite food here</h2>
+          <p>
+            Choose from a diverse menu featuring delectable array of deshes
+            crafted with finest ingredients and culinary expertise. Our missiion
+            is to satisfy your cravings and elevate your dining experience, one
+            delicious meal at a time{" "}
+          </p>
+          <button>View Menu</button>
+        </div>
+      </div>
+    </>
+  );
 };
 import "./Header.css";
 
