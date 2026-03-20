7b8ea512a440a300968ab9b5d8605d277c305b1e
khushi-7488 <khushiitiwari88@gmail.com>
2026-03-20 15:59:25 +0530

    build login and signup functionality

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "./Login.css";
+import { useState } from "react";
 
 const Login = () => {
-  const [currState, setCurrentState] = useState("Sign Up");
+  const [currState, setCurrentState] = useState("Sign up");
 
   return (
     <div className="login">
       <form action="" className="form">
         <div className="card">
           <h2>{currState}</h2>
-          {currState === "Sign Up" ? (
+          {currState === "Login" ? (
             <></>
           ) : (
             <input type="text" placeholder="Your name" />
@@ -17,11 +18,32 @@ const Login = () => {
           <input type="email" placeholder="Your email" />
           <input type="password" placeholder="Your password" />
 
-          <button>Sign Up</button>
+          <button>
+            {currState === "Sign up" ? "Create Account" : "Login"}
+          </button>
           <div className="login-popup-condition">
             <input type="checkbox" name="" id="" />
             <p>by continuing, i agree to the terms and conditions</p>
           </div>
+          {currState === "Sign up" ? (
+            <div>
+              <p>
+                Already have an account ?{" "}
+                <span onClick={() => setCurrentState("Login")}>
+                  Login here
+                </span>{" "}
+              </p>
+            </div>
+          ) : (
+            <div>
+              <p>
+                create a new account ?{" "}
+                <span onClick={() => setCurrentState("Sign up")}>
+                  Click here
+                </span>{" "}
+              </p>
+            </div>
+          )}
         </div>
       </form>
     </div>
