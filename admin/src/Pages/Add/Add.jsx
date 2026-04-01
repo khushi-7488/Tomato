69a17e9cda21ecf1b87ee177136b7fb3a94a3fbe
khushi-7488 <khushiitiwari88@gmail.com>
2026-04-01 23:50:28 +0530

    build functionality of add.jsx

--- admin/src/Pages/Add/Add.jsx
+++ admin/src/Pages/Add/Add.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
 import "./Add.css";
 import { assets } from "../../assets/assets";
+import axios from "axios";
 
 const Add = () => {
+  const url = "http://localhost:4080";
   const [image, setImage] = useState(false);
   const [data, setData] = useState({
     name: "",
     description: "",
-    category: "",
     price: "",
+    category: "Salad",
   });
 
   const onChangeHandler = (event) => {
@@ -16,8 +18,22 @@ const Add = () => {
     const value = event.target.value;
     setData((data) => ({ ...data, [name]: value }));
   };
+
   const onSubmitHandler = async (event) => {
     event.preventDefault();
+    const formData = new FormData();
+    formData.append("name", data.name);
+    formData.append("description", data.description);
+    formData.append("price", Number(data.price));
+    formData.append("category", data.category);
+    formData.append("image", image);
+
+    const response = await axios.post(`${url}/api/food/add`, formData);
+
+    if (response.data.success) {
+      setData({ name: "", description: "", price: "", category: "Salad" });
+      setImage(false);
+    }
   };
   // useEffect(() => {
   //   console.log(data);
