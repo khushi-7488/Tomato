bd0cc618560800c1e9f601bc509eb675b41654fb
khushi-7488 <khushiitiwari88@gmail.com>
2026-04-03 18:52:48 +0530

    build user model and controller

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,2 +1,12 @@
 import userModel from "../models/userModel";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    const user = userModel.find({ email });
+    if (!user) {
+        res.json({ success: false, message: "User not found" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+}
